3009df278a49cf3d0780084f9d4d2fadb876055c
Carlos Aguilar <carlos.aguilarcampos@outlook.com>
2024-09-27 01:43:08 -0600

    Adding JOINs information to DMl sentences SQL file

--- Queries/DML sentences.sql
+++ Queries/DML sentences.sql
@@ -16,3 +16,17 @@ UPDATE Person SET Name = 'Francisco' where personid = 3
 
 --DELETE 
 DELETE Person WHERE PersonID = 3
+
+/* 
+	JOINS:
+	1. Inner Join
+	2. Left Join
+*/
+
+-- 1. Inner Join
+SELECT p.PersonID,p.Name,i.Completed
+FROM Items i INNER JOIN Person P ON i.PersonID = p.PersonID 
+
+-- 2. Left Join
+SELECT p.PersonID,p.Name,i.Completed
+FROM Items i INNER JOIN Person P ON i.PersonID = p.PersonID 
